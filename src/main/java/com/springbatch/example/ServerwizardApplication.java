714a0241bb339355bcb3810e6e3054a8ae36602b
홍종완 <serverwizard89@gmail.com>
2017-09-13 02:26:22 +0900

    Spring Batch Basic Example

---
+++ src/main/java/com/springbatch/example/ServerwizardApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerwizardApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServerwizardApplication.class, args);
+    }
+}
---
+++ src/main/java/com/springbatch/example/domain/Person.java
@@ -0,0 +1,40 @@
+package com.springbatch.example.domain;
+
+/**
+ * Created by hongjong-wan on 2017. 9. 13..
+ */
+public class Person {
+    private String lastName;
+    private String firstName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + ", lastName: " + lastName;
+    }
+
+}
